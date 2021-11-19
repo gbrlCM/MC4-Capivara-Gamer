bb90d995c837639b2fbe1b03605791906039b5bf
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-11-19 17:32:32 -0300

    Initial Commit

---
+++ Capivara/CapivaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapivaraApp.swift
+//  Capivara
+//
+//  Created by Gabriel Ferreira de Carvalho on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CapivaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
