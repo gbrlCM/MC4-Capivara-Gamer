ce8b10dca2907c00bcb98f7f016d206846c97cdc
thallissousa <thallissousa@outlook.com>
2021-11-24 17:54:50 -0300

    Geometry reader added and Search Results

--- Capivara/Screen/MyEvents/Views/MyEventsView.swift
+++ Capivara/Screen/MyEvents/Views/MyEventsView.swift
@@ -10,25 +10,44 @@ import SwiftUI
 struct MyEventsView: View {
     
     @State private var searchText = ""
+    @State private var content = ["aa", "bb", "cc", "dd", "ee", "paçoquinha", "pudim"]
     
     var body: some View {
         NavigationView {
+            GeometryReader { proxy in
+                List {
+                    ForEach(searchResults, id: \.self){ content in
+                        EventCardView(text: content)
+                            .frame(width: proxy.size.width, height: proxy.size.width*0.3)
+                            .background(Color.pink)
+                            .padding(.bottom, 4)
+                        
+                    }.listRowSeparator(.hidden)
+                }
+                .listStyle(.plain)
+                
+            }
             
-            VStack {
-                Text(searchText)
-            }.toolbar{
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: {} , label: {
-                            Image(systemName: "plus")
-                        })
-                    }
-            }.navigationTitle("Eventos")
-                .searchable(text: $searchText)
-        }
-        
-        .tabBarLabel(text: "Eventos", systemImage: "newspaper.fill")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {} , label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            }
+            .navigationTitle("Eventos")
+            
+        }.searchable(text: $searchText)
+            .tabBarLabel(text: "Eventos", systemImage: "newspaper.fill")
     }
     
+    var searchResults: [String] {
+        if searchText.isEmpty{
+            return content
+        }else{
+            return content.filter{$0.contains(searchText)}
+        }
+    }
 }
 
 struct MyEventsView_Previews: PreviewProvider {
@@ -36,5 +55,15 @@ struct MyEventsView_Previews: PreviewProvider {
         TabView {
             MyEventsView()
         }
+    }
+}
+
+
+struct EventCardView: View {
+    var text: String
+    
+    var body: some View{
+        Text(text)
+            
     }
 }
