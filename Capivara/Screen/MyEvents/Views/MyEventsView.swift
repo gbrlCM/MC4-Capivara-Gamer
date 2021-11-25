0fc3b66eb8c8b4cecbefc16f098eb5337b6dce32
VitorCheung <Vitorcheung@gmail.com>
2021-11-25 18:05:28 -0300

    ViewComRadius

--- Capivara/Screen/MyEvents/Views/MyEventsView.swift
+++ Capivara/Screen/MyEvents/Views/MyEventsView.swift
@@ -14,19 +14,19 @@ struct MyEventsView: View {
     
     var body: some View {
         NavigationView {
-            GeometryReader { proxy in
-                List {
-                    ForEach(searchResults, id: \.self){ content in
-                        EventCardView(text: content)
-                            .frame(width: proxy.size.width, height: proxy.size.width*0.3)
-                            .background(Color.pink)
-                            .padding(.bottom, 4)
-                        
-                    }.listRowSeparator(.hidden)
-                }
-                .listStyle(.plain)
-                
+            
+            List {
+                ForEach(searchResults, id: \.self){ content in
+                    EventCardView(text: content)
+                        .frame(maxWidth: .infinity, minHeight: 155, maxHeight: 155)
+                    
+                    
+                    
+                }.listRowSeparator(.hidden)
             }
+            .listStyle(.plain)
+            
+            
             
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -63,7 +63,60 @@ struct EventCardView: View {
     var text: String
     
     var body: some View{
-        Text(text)
-            
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.gray)
+            VStack(alignment: .trailing){
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 50, height: 50)
+                    .opacity(0.6)
+                    .cornerRadius(10 ,corners: .topRight)
+                    .cornerRadius(30 ,corners: .bottomLeft)
+                Spacer()
+                ZStack(alignment: .leading){
+                    Rectangle()
+                        .fill(Color.black)
+                        .opacity(0.6)
+                        .frame(maxWidth: .infinity, minHeight: 60, maxHeight: 60)
+                        .cornerRadius(10, corners: [.bottomLeft,.bottomRight])
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text(text)
+                                .padding(.leading)
+                                .font(.system(size: 24.0))
+                                .foregroundColor(Color.blue)
+                            Text("Data")
+                                .padding(.leading)
+                                .foregroundColor(Color.blue)
+                        }
+                        Spacer()
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 30, height: 30)
+                            .padding(.trailing)
+
+                    }
+                }
+            }
+        }
     }
+}
+
+struct RoundedCorner: Shape{
+    
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View{
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View{
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+    
 }
