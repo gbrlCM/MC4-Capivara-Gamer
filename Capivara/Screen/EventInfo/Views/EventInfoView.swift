d0a630f99e85ca6857ea25a373adc250cf7407ee
Nathalia Papst <nathaliapapst@gmail.com>
2021-11-30 16:42:58 -0300

    Configurar arquivo

---
+++ Capivara/Screen/EventInfo/Views/EventInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  EventInfoView.swift
+//  Capivara
+//
+//  Created by Nathalia do Valle Papst on 30/11/21.
+//
+
+import SwiftUI
+
+struct EventInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EventInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        EventInfoView()
+    }
+}
