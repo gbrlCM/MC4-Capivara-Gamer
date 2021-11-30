752d40b67e70987812639ed26fd49be1f7e7b145
Nathalia Papst <nathaliapapst@gmail.com>
2021-11-30 17:12:53 -0300

    Irá adicionar uma navigation bar e um small title

--- Capivara/Screen/EventInfo/Views/EventInfoView.swift
+++ Capivara/Screen/EventInfo/Views/EventInfoView.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct EventInfoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+            }.navigationTitle("Informação do Evento")
+             .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 struct EventInfoView_Previews: PreviewProvider {
     static var previews: some View {
         EventInfoView()
+        EventInfoView()
+            .previewDevice("iPhone 8")
+        EventInfoView()
+        .previewDevice("iPhone SE (2nd generation)")
     }
 }
