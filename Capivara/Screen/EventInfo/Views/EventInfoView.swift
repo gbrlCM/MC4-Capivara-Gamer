c5f56e855ff3db80352fab03cef1725a339e163d
Nathalia Papst <nathaliapapst@gmail.com>
2021-11-30 17:33:58 -0300

    Irá adicionar uma VStack

--- Capivara/Screen/EventInfo/Views/EventInfoView.swift
+++ Capivara/Screen/EventInfo/Views/EventInfoView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct EventInfoView: View {
     var body: some View {
         NavigationView {
-            Form {
+            VStack {
             }.navigationTitle("Informação do Evento")
              .navigationBarTitleDisplayMode(.inline)
         }
