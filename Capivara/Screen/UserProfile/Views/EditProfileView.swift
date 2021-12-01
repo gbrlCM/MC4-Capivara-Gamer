86d3062d9e65d952a9f5ba27cb8ec5d23b7a10fb
Nathalia Papst <nathaliapapst@gmail.com>
2021-12-01 16:42:54 -0300

    Adicionar navigation bar e large title

---
+++ Capivara/Screen/UserProfile/Views/EditProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  EditProfileView.swift
+//  Capivara
+//
+//  Created by Nathalia do Valle Papst on 01/12/21.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+            }.navigationTitle("Editar Perfil")
+             . 
+        }
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
