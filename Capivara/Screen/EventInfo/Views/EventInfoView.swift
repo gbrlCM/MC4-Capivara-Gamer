8f6ea3ee24a411deda632bbfc8c9ae77108563de
Nathalia Papst <nathaliapapst@gmail.com>
2021-12-02 17:36:05 -0300

    Adicionar view com informações do jogo e da plataforma

--- Capivara/Screen/EventInfo/Views/EventInfoView.swift
+++ Capivara/Screen/EventInfo/Views/EventInfoView.swift
@@ -12,13 +12,68 @@ struct EventInfoView: View {
         NavigationView {
             VStack {
                 ScrollView {
-                    Image("Celulol")
-                        .resizable()
-                        .frame(height: 200)
-                }
+                    ZStack {
+                        Image("Celulol")
+                            .resizable()
+                        .frame(height: 230)
+                        
+                        VStack {
+                            Spacer()
+                            
+                            HStack(alignment: .bottom) {
+                                VStack(alignment: .leading) {
+                                    Text("Campeonato de LOL")
+                                        .font(.system(size: 24, weight: .bold, design: .default))
+                                    Text("24/11/2021 | 15:30")
+                                }.foregroundColor(ColorPalette.primaryText)
+                                 .padding()
+                                
+                                Spacer()
+                                
+                                Button(action:{}) {
+                                    Text("Participar")
+                                }.foregroundColor(ColorPalette.primaryText)
+                                 .frame(width: 90, height: 30)
+                                 .background(ColorPalette.accent)
+                                 .cornerRadius(21)
+                                 .padding()
+                            }.background(ColorPalette.backgroundColor.opacity(0.9))
+                        }
+                    }
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image("CelulolLogo")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .cornerRadius(50)
+                                
+                                Text("League Of Legends")
+                                    .foregroundColor(ColorPalette.primaryText)
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            
+                            HStack {
+                                Image(systemName: "logo.playstation")
+                                
+                                Text("PS4")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }.foregroundColor(ColorPalette.primaryText)
+                        }.padding()
+                         .background(ColorPalette.secondaryBackground)
+                         .cornerRadius(10)
+                         .padding()
+                            
+                        Button(action:{}) {
+                            Image("")
+                        }
+                        Spacer()
+                    }
             }.backgroundColor(ColorPalette.backgroundColor)
              .navigationTitle("Informação do Evento").navigationBarTitleColor(ColorPalette.primaryText)
              .navigationBarTitleDisplayMode(.inline)
+            }
         }
         
         ScrollView {
