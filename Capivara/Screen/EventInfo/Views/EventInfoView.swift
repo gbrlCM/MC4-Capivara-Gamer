302e4420c7a6610b82acd9a95e72ce4844ac56cb
Nathalia Papst <nathaliapapst@gmail.com>
2021-12-02 17:53:41 -0300

    Adicionar botão do discord e arrumar a navigation view

--- Capivara/Screen/EventInfo/Views/EventInfoView.swift
+++ Capivara/Screen/EventInfo/Views/EventInfoView.swift
@@ -9,85 +9,85 @@ import SwiftUI
 
 struct EventInfoView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                ScrollView {
-                    ZStack {
-                        Image("Celulol")
-                            .resizable()
+        VStack {
+            ScrollView {
+                ZStack {
+                    Image("Celulol")
+                        .resizable()
                         .frame(height: 230)
+                    
+                    VStack {
+                        Spacer()
                         
-                        VStack {
+                        HStack(alignment: .bottom) {
+                            VStack(alignment: .leading) {
+                                Text("Campeonato de LOL")
+                                    .font(.system(size: 24, weight: .bold, design: .default))
+                                Text("24/11/2021 | 15:30")
+                            }.foregroundColor(ColorPalette.primaryText)
+                                .padding()
+                            
                             Spacer()
                             
-                            HStack(alignment: .bottom) {
-                                VStack(alignment: .leading) {
-                                    Text("Campeonato de LOL")
-                                        .font(.system(size: 24, weight: .bold, design: .default))
-                                    Text("24/11/2021 | 15:30")
-                                }.foregroundColor(ColorPalette.primaryText)
-                                 .padding()
-                                
-                                Spacer()
-                                
-                                Button(action:{}) {
-                                    Text("Participar")
-                                }.foregroundColor(ColorPalette.primaryText)
-                                 .frame(width: 90, height: 30)
-                                 .background(ColorPalette.accent)
-                                 .cornerRadius(21)
-                                 .padding()
-                            }.background(ColorPalette.backgroundColor.opacity(0.9))
-                        }
-                    }
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Image("CelulolLogo")
-                                    .resizable()
-                                    .frame(width: 30, height: 30)
-                                    .cornerRadius(50)
-                                
-                                Text("League Of Legends")
-                                    .foregroundColor(ColorPalette.primaryText)
-                                    .font(.system(size: 14, weight: .semibold))
-                            }
-                            
-                            HStack {
-                                Image(systemName: "logo.playstation")
-                                
-                                Text("PS4")
-                                    .font(.system(size: 14, weight: .semibold))
+                            Button(action:{}) {
+                                Text("Participar")
                             }.foregroundColor(ColorPalette.primaryText)
-                        }.padding()
-                         .background(ColorPalette.secondaryBackground)
-                         .cornerRadius(10)
-                         .padding()
-                            
-                        Button(action:{}) {
-                            Image("")
-                        }
-                        Spacer()
+                                .frame(width: 90, height: 30)
+                                .background(ColorPalette.accent)
+                                .cornerRadius(21)
+                                .padding()
+                        }.background(ColorPalette.backgroundColor.opacity(0.9))
                     }
+                }
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image("CelulolLogo")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .cornerRadius(50)
+                            
+                            Text("League Of Legends")
+                                .foregroundColor(ColorPalette.primaryText)
+                                .font(.system(size: 14, weight: .semibold))
+                        }
+                        
+                        HStack {
+                            Image(systemName: "logo.playstation")
+                            
+                            Text("PS4")
+                                .font(.system(size: 14, weight: .semibold))
+                        }.foregroundColor(ColorPalette.primaryText)
+                    }.padding()
+                        .background(ColorPalette.secondaryBackground)
+                        .cornerRadius(10)
+                        .padding()
+                    
+                    Button(action:{}) {
+                        Image("DiscordRosa")
+                            .resizable()
+                            .frame(width: 35, height: 30)
+                    }.padding()
+                     .background(ColorPalette.accent.opacity(0.4))
+                     .cornerRadius(50)
+                    Spacer()
+                }
             }.backgroundColor(ColorPalette.backgroundColor)
-             .navigationTitle("Informação do Evento").navigationBarTitleColor(ColorPalette.primaryText)
-             .navigationBarTitleDisplayMode(.inline)
-            }
-        }
-        
-        ScrollView {
-            Text("oi").foregroundColor(.white)
+                .navigationTitle("Informação do Evento").navigationBarTitleColor(ColorPalette.primaryText)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 struct EventInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        EventInfoView()
-        EventInfoView()
-            .previewDevice("iPhone 8")
-        EventInfoView()
-        .previewDevice("iPhone SE (2nd generation)")
+        NavigationView {
+            EventInfoView()
+            EventInfoView()
+                .previewDevice("iPhone 8")
+            EventInfoView()
+                .previewDevice("iPhone SE (2nd generation)")
+        }
     }
 }
