9bc91399591c22ae04e9fac4eeaf1d5076c235fe
Nathalia Papst <nathaliapapst@gmail.com>
2021-12-02 15:21:50 -0300

    Adicionar elementos visuais iniciais

--- Capivara/Screen/UserProfile/Views/EditProfileView.swift
+++ Capivara/Screen/UserProfile/Views/EditProfileView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    @ObservedObject var viewModel: UserProfileViewModel
+    @State private var username: String = ""
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .center) {
+                UserProfileAvatar(imageURL: $viewModel.user.avatar)
                 
+                Button(action: {}) {
+                    Label("Editar", systemImage: "pencil")
+                }
+                
+                TextField("Username", text: $username)
+                    .foregroundColor(ColorPalette.primaryText)
+                    .frame(width: 350, height: 40, alignment: .center)
+                    .background(ColorPalette.secondaryBackground)
+                    .cornerRadius(10)
+                    
+                HStack {
+                    Text("Data de Nascimento")
+                        .foregroundColor(ColorPalette.primaryText)
+                    Spacer()
+                }.padding()
+                    
             }.navigationTitle("Editar Perfil")
-             . 
+             .navigationBarTitleDisplayMode(.inline)
+             .navigationBarTitleColor(ColorPalette.primaryText)
+             .backgroundColor(ColorPalette.backgroundColor)
+             
         }
     }
 }
 
 struct EditProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        EditProfileView()
+        EditProfileView(viewModel: UserProfileViewModel())
     }
 }
