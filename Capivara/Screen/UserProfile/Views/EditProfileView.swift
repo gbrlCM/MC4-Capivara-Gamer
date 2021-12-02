01ef4066f2ab72ba8a9106eaf80db56ce61dc311
DeboraKass <83611317+DeboraKass@users.noreply.github.com>
2021-12-02 18:03:05 -0300

    arrumação do código e posição dos itens

--- Capivara/Screen/UserProfile/Views/EditProfileView.swift
+++ Capivara/Screen/UserProfile/Views/EditProfileView.swift
@@ -11,37 +11,69 @@ struct EditProfileView: View {
     @ObservedObject var viewModel: UserProfileViewModel
     @State private var username: String = ""
     var body: some View {
+        
+        VStack() {
+            userPhotoChanger
+                            
+            userNameChanger
+                .padding()
+            
+            userDateChanger
+                
+               
+            
+            Spacer()
+            
+        }.navigationTitle("Editar Perfil")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitleColor(ColorPalette.primaryText)
+            .backgroundColor(ColorPalette.backgroundColor)
+        
+    }
+    
+    @ViewBuilder
+    var userPhotoChanger: some View {
+        VStack {
+            UserProfileAvatar(imageURL: $viewModel.user.avatar)
+            
+            Button(action: {}) {
+                Label("Editar", systemImage: "pencil")
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var userNameChanger: some View {
+        VStack {
+            TextField("Username", text: $username)
+                .foregroundColor(ColorPalette.primaryText)
+                .frame(width: 350, height: 40, alignment: .center)
+                .background(ColorPalette.secondaryBackground)
+                .cornerRadius(10)
+        }
+    }
+    
+    @ViewBuilder
+    var userDateChanger: some View {
+        HStack() {
+            Text("Data de Nascimento")
+                .foregroundColor(ColorPalette.primaryText)
+            Spacer()
+        }.padding()
+        
+        DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { Text("") })
+            .labelsHidden()
+            
+    }
+}
+
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
         NavigationView {
-            VStack(alignment: .center) {
-                UserProfileAvatar(imageURL: $viewModel.user.avatar)
-                
-                Button(action: {}) {
-                    Label("Editar", systemImage: "pencil")
-                }
-                
-                TextField("Username", text: $username)
-                    .foregroundColor(ColorPalette.primaryText)
-                    .frame(width: 350, height: 40, alignment: .center)
-                    .background(ColorPalette.secondaryBackground)
-                    .cornerRadius(10)
-                    
-                HStack {
-                    Text("Data de Nascimento")
-                        .foregroundColor(ColorPalette.primaryText)
-                    Spacer()
-                }.padding()
-                    
-            }.navigationTitle("Editar Perfil")
-             .navigationBarTitleDisplayMode(.inline)
-             .navigationBarTitleColor(ColorPalette.primaryText)
-             .backgroundColor(ColorPalette.backgroundColor)
-             
+            EditProfileView(viewModel: UserProfileViewModel())
         }
     }
 }
 
-struct EditProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        EditProfileView(viewModel: UserProfileViewModel())
-    }
-}
+
