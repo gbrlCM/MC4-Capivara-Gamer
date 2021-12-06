dfb96f945f8f83fe5c61526c8073e4b609498340
VitorCheung <Vitorcheung@gmail.com>
2021-12-06 13:50:23 -0300

    ageitando struct

--- Capivara/Screen/MyEvents/Views/EventCardView.swift
+++ Capivara/Screen/MyEvents/Views/EventCardView.swift
@@ -63,23 +63,6 @@ struct EventCardView: View {
     }
 }
 
-struct RoundedCorner: Shape{
-    
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
-
-extension View{
-    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View{
-        clipShape(RoundedCorner(radius: radius, corners: corners))
-    }
-}
-
 struct EventCardView_Previews: PreviewProvider {
     static var previews: some View {
         TabView {
