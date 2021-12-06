bee24425c0e9fcc49f349cc074946fe7e6038902
thallissousa <thallissousa@outlook.com>
2021-12-06 14:00:26 -0300

    minor updates

--- Capivara/Screen/ViewModel/SignInWApple.swift
+++ Capivara/Screen/ViewModel/SignInWApple.swift
@@ -20,45 +20,46 @@ struct SignInWApple: View {
         NavigationView{
             VStack{
                 SignInWithAppleButton(.continue) { request in
-                    
                     request.requestedScopes = [.email, .fullName]
-            
+                } onCompletion: { result in
+                    
+                    switch result {
+                    case .success(let auth):
+                        
+                        switch auth.credential {
+                        case let Credential as ASAuthorizationAppleIDCredential:
+                            
+                            //user ID
+                            let userId = Credential.user
+                            
+                            //User Infos
+                            let email = Credential.email
+                            let firstName = Credential.fullName?.givenName
+                            let lastName = Credential.fullName?.familyName
+                            
+                            self.email = email ?? ""
+                            self.userId = userId
+                            self.firstName = firstName ?? ""
+                            self.lastName = lastName ?? ""
+                            
+                        default:
+                            break
+                            
+                        }
+                    case .failure(let error):
+                        print(error)
+                    }
                 }
-                                      onCompletion: { result in
-                                          
-                                          switch result {
-                                          case .success(let auth):
-                                              
-                                              switch auth.credential {
-                                              case let credential as ASAuthorizationAppleIDCredential:
-                                                  
-                                                  //user ID
-                                                  let userId = credential.user
-                                                  
-                                                  //User Infos
-                                                  let email = credential.email
-                                                  let firstName = credential.fullName?.givenName
-                                                  let lastName = credential.fullName?.familyName
-                                                  
-                                                  self.email = email ?? ""
-                                                  self.userId = userId
-                                                  self.firstName = firstName ?? ""
-                                                  self.lastName = lastName ?? ""
-                                                  
-                                              default:
-                                                  break
-                                                
-                                              }
-                                          case .failure(let error):
-                                              print(error)
-                                          }
-                }
-                                      .frame(width: 280, height: 60)
-                                      .padding()
-                                      .cornerRadius(8)            }
+                signInWithAppleButtonStyle(
+                    colorScheme == .dark ? .white: .black)
+
+                
+                .frame(width: 280, height: 60)
+                .padding()
+                .cornerRadius(8)            }
             .navigationTitle("Sign In")
         }
-
+        
     }
 }
 
