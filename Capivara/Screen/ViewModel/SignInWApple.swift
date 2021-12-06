8377f3539e5b9ac85cbf37a7adc6b87adf070da0
thallissousa <thallissousa@outlook.com>
2021-12-06 13:38:53 -0300

    alterações no botão de login

--- Capivara/Screen/ViewModel/SignInWApple.swift
+++ Capivara/Screen/ViewModel/SignInWApple.swift
@@ -11,35 +11,55 @@ import AuthenticationServices
 struct SignInWApple: View {
     @Environment(\.colorScheme) var colorScheme
     
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
     var body: some View {
         NavigationView{
             VStack{
                 SignInWithAppleButton(.continue) { request in
+                    
+                    request.requestedScopes = [.email, .fullName]
             
                 }
                                       onCompletion: { result in
+                                          
+                                          switch result {
+                                          case .success(let auth):
+                                              
+                                              switch auth.credential {
+                                              case let credential as ASAuthorizationAppleIDCredential:
+                                                  
+                                                  //user ID
+                                                  let userId = credential.user
+                                                  
+                                                  //User Infos
+                                                  let email = credential.email
+                                                  let firstName = credential.fullName?.givenName
+                                                  let lastName = credential.fullName?.familyName
+                                                  
+                                                  self.email = email ?? ""
+                                                  self.userId = userId
+                                                  self.firstName = firstName ?? ""
+                                                  self.lastName = lastName ?? ""
+                                                  
+                                              default:
+                                                  break
+                                                
+                                              }
+                                          case .failure(let error):
+                                              print(error)
+                                          }
                 }
                                       .frame(width: 280, height: 60)
                                       .padding()
-                                      .cornerRadius(8)
-                                      .onTapGesture(perform: showAppleLogin)
-            }
+                                      .cornerRadius(8)            }
             .navigationTitle("Sign In")
         }
 
     }
-    
-    private func showAppleLogin() {
-      // 1
-      let request = ASAuthorizationAppleIDProvider().createRequest()
-
-      // 2
-      request.requestedScopes = [.fullName, .email]
-
-      // 3
-      let controller = ASAuthorizationController(authorizationRequests: [request])
-    }
-    
 }
 
 struct SignInWApple_Previews: PreviewProvider {
