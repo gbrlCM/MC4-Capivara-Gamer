371c7d4817d5e217abed051b77fcc892825d629b
thallissousa <thallissousa@outlook.com>
2021-12-06 15:30:58 -0300

    Inserção do botão na LoginView e alterações de estilo

--- Capivara/Screen/LoginView.swift
+++ Capivara/Screen/LoginView.swift
@@ -10,29 +10,28 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         ZStack {
-            VStack {
-                Text("Bem vindo ao CapivaraGamer!")
-                    .font(.title.bold())
-                Text("O aplicativo que te ajuda a encontrar capivaras pro players de COD!")
-                    .font(.title2)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Spacer()
+            VStack (alignment: .center) {
+                VStack (alignment: .leading){
+                    Text("Bem vindo ao CapivaraGamer!")
+                        .font(.title.bold())
+                    
+                    Text("O aplicativo que te ajuda a encontrar capivaras pro players de COD!")
+                        .font(.title2)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                }.foregroundColor(ColorPalette.primaryText)
+                    .padding()
                 SignInWApple()
-
-            }.foregroundColor(ColorPalette.primaryText)
-                .padding()
-            
+            }
             Image("Loginbara")
                 .resizable()
                 .scaledToFit()
         }.backgroundColor(ColorPalette.backgroundColor)
-        
-        
     }
 }
-    
-    struct LoginView_Previews: PreviewProvider {
-        static var previews: some View {
-            LoginView()
-        }
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
     }
+}
--- Capivara/Screen/ViewModel/SignInWApple.swift
+++ Capivara/Screen/ViewModel/SignInWApple.swift
@@ -9,49 +9,48 @@ import SwiftUI
 import AuthenticationServices
 
 struct SignInWApple: View {
-    @Environment(\.colorScheme) var colorScheme
     @AppStorage("email") var email: String = ""
     @AppStorage("firstName") var firstName: String = ""
     @AppStorage("lastName") var lastName: String = ""
     @AppStorage("userId") var userId: String = ""
     
     var body: some View {
-                SignInWithAppleButton(.continue) { request in
-                    request.requestedScopes = [.email, .fullName]
-                } onCompletion: { result in
+        SignInWithAppleButton(.continue) { request in
+            request.requestedScopes = [.email, .fullName]
+        } onCompletion: { result in
+            
+            switch result {
+            case .success(let auth):
+                
+                switch auth.credential {
+                case let Credential as ASAuthorizationAppleIDCredential:
                     
-                    switch result {
-                    case .success(let auth):
-                        
-                        switch auth.credential {
-                        case let Credential as ASAuthorizationAppleIDCredential:
-                            
-                            let userId = Credential.user
-                            let email = Credential.email
-                            let firstName = Credential.fullName?.givenName
-                            let lastName = Credential.fullName?.familyName
-                            
-                            self.email = email ?? ""
-                            self.userId = userId
-                            self.firstName = firstName ?? ""
-                            self.lastName = lastName ?? ""
-                            
-                            print(userId)
-                            
-                        default:
-                            break
-                            
-                        }
-                    case .failure(let error):
-                        print(error)
-                    }
+                    let userId = Credential.user
+                    let email = Credential.email
+                    let firstName = Credential.fullName?.givenName
+                    let lastName = Credential.fullName?.familyName
+                    
+                    self.email = email ?? ""
+                    self.userId = userId
+                    self.firstName = firstName ?? ""
+                    self.lastName = lastName ?? ""
+                    
+                    print(userId)
+                    
+                default:
+                    break
+                    
                 }
-                .signInWithAppleButtonStyle(
-                    colorScheme == .dark ? .white: .white)
-                .frame(width: 300, height: 50)
-                .padding()
-                .cornerRadius(8)            }
+            case .failure(let error):
+                print(error)
+            }
         }
+        .signInWithAppleButtonStyle(.whiteOutline)
+        .frame(width: 300, height: 50)
+        .padding()
+        .cornerRadius(8)
+    }
+}
 
 struct SignInWApple_Previews: PreviewProvider {
     static var previews: some View {
