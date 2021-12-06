54412301c22514ba8000c4a1f7bf6e4834c2bbd3
DeboraKass <83611317+DeboraKass@users.noreply.github.com>
2021-12-06 17:33:11 -0300

    tela de explorar

--- Capivara/Screen/Search/Views/SearchView.swift
+++ Capivara/Screen/Search/Views/SearchView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var searchFieldContent: String = ""
     var body: some View {
+        NavigationView {
+            VStack{
+                searchBar
+            } .backgroundColor(ColorPalette.backgroundColor)
+                .navigationTitle("Explorar")
+                .navigationBarTitleColor(ColorPalette.primaryText)
+        }
         Text("Search")
             .tabBarLabel(text: "Buscar", systemImage: "magnifyingglass")
     }
+    
+    @ViewBuilder
+    var searchBar: some View {
+        
+        TextField("Search ...", text: $searchFieldContent)
+            .padding(7)
+            .padding(.horizontal, 25)
+            .background(Color(.systemGray6))
+            .cornerRadius(8)
+            .padding(.horizontal, 10)
+    }
 }
+
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
