1e5c630df56055be80553d79aa232d9cc6661dcf
VitorCheung <Vitorcheung@gmail.com>
2021-12-07 13:58:32 -0300

    finalizar share

---
+++ Capivara/Screen/MyEvents/MyEventsTab 2.swift
@@ -0,0 +1,15 @@
+//
+//  MyEventsTab.swift
+//  Capivara
+//
+//  Created by Thallis Sousa on 02/12/21.
+//
+
+import Foundation
+
+enum MyEventsTab: String, CaseIterable {
+    case all = "Todos"
+    case current = "Atuais"
+    case created = "Criados"
+    case participated = "Participados"
+}
