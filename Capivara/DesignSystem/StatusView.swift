43d0a6c27c6e1ca4e3c4b7aa8134aa756c9422a2
VitorCheung <Vitorcheung@gmail.com>
2021-12-10 17:30:05 -0300

    StatusView

---
+++ Capivara/DesignSystem/StatusView.swift
@@ -0,0 +1,15 @@
+//
+//  statusView.swift
+//  Capivara
+//
+//  Created by Vitor Cheung on 10/12/21.
+//
+
+import Foundation
+
+enum StatusView: String, CaseIterable {
+    case erro = "Erro"
+    case loading = "Loading"
+    case empy = "Empy"
+    case Ok = "Ok"
+}
