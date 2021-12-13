69379d61685a8bed6d755b5cfad282f1b6159264
Gabriel Ferreira de Carvalho <gabrielfcarvalho360@icloud.com>
2021-12-13 15:00:49 -0300

    authentication implemented

--- Capivara/Screen/Login/ViewModel/LoginViewModel.swift
+++ Capivara/Screen/Login/ViewModel/LoginViewModel.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import AuthenticationServices
 
 final class LoginViewModel: ObservableObject {
-    @AppStorage("id") var userId: String = ""
     
     @Published var didOccurError: Bool
     
@@ -18,22 +17,6 @@ final class LoginViewModel: ObservableObject {
     init(repository: UserRepositoryProtocol){
         self.repository = repository
         self.didOccurError = false
-    }
-    
-    func loginUser(id: String, username: String, dateOfBirth: Date) -> User{
-        let user = User(id: id, username: username, avatar: "", description: "", dateOfBirth: dateOfBirth)
-        userId = user.id
-        return user
-    }
-    
-    func checkIfIsLogged() {
-        let id = userId
-        if id != "" {
-            print("Seja bem vindo novamente, Capi")
-            //se estiver logado, é aqui
-        } else {
-            //caso não esteja logado, é aqui
-        }
     }
     
     func onLoginCompletion(result: Result<ASAuthorization, Error>) {
@@ -57,15 +40,15 @@ final class LoginViewModel: ObservableObject {
         let fullName = "\(firstName ?? "") \(lastName ?? "")"
         let loginData = LoginData(appleId: userId, username: fullName)
         
-        Task {
-            do {
-                try await repository.login(loginData)
-                let user = try await repository.retrieveUser(userId: "")
-                print(user)
-            } catch {
-                didOccurError = true
-            }
-        }
+//        Task {
+//            do {
+//                try await repository.login(loginData)
+//                let user = try await repository.retrieveUser(userId: "")
+//                print(user)
+//            } catch {
+//                didOccurError = true
+//            }
+//        }
     }
     
 }
---
+++ Capivara/Services/Authentication/AuthenticationService.swift
@@ -0,0 +1,84 @@
+//
+//  AuthenticationService.swift
+//  Capivara
+//
+//  Created by Gabriel Ferreira de Carvalho on 13/12/21.
+//
+
+import Foundation
+import AuthenticationServices
+
+final class AutheticationService {
+    
+    @Published
+    private(set) var loggedUser: User?
+    
+    private let userRepository: UserRepositoryProtocol
+    private let keychainService: KeyChainServiceProtocol
+    
+    private let credentialKey = "userCredential"
+    private let serviceKey = "com.gbrlcm.capivara"
+    
+    init(repository: UserRepositoryProtocol, keychainService: KeyChainServiceProtocol) {
+        self.loggedUser = nil
+        self.userRepository = repository
+        self.keychainService = keychainService
+    }
+    
+    func signIn(credentials :ASAuthorizationAppleIDCredential) async throws {
+        let userId = credentials.user
+        let firstName = credentials.fullName?.givenName
+        let lastName = credentials.fullName?.familyName
+        let fullName = "\(firstName ?? "") \(lastName ?? "")"
+        
+        let newUser = User(id: nil,
+                           username: fullName,
+                           avatar: "",
+                           description: "",
+                           dateOfBirth: Date(),
+                           appleId: userId)
+        
+        do {
+            try keychainService.set(userId, forKey: credentialKey, inService: serviceKey)
+            try await userRepository.login(newUser)
+        } catch {
+            throw error
+        }
+    }
+    
+    func login() async throws {
+        do {
+            let userId = try keychainService.get(forKey: credentialKey, inService: serviceKey)
+            loggedUser = try await userRepository.retrieveUser(userId: userId)
+        } catch {
+            throw error
+        }
+    }
+    
+    func logout() throws {
+        guard loggedUser != nil else {
+            throw NSError(domain: "User not loggedIn", code: 3, userInfo: nil)
+        }
+        
+        do {
+            try keychainService.delete(forKey: credentialKey, inService: serviceKey)
+            self.loggedUser = nil
+        } catch  {
+            throw error
+        }
+    }
+    
+    func deleteAccount() async throws {
+        guard let loggedUser = loggedUser else {
+            return
+        }
+        
+        do {
+            try logout()
+            try await userRepository.deleteUser(userId: loggedUser.appleId)
+        } catch {
+            throw error
+        }
+    }
+    
+}
