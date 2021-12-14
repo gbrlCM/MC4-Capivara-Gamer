b48f36758389a9af72f8c6d82aa9b42ab10bfd9b
Nathalia Papst <nathaliapapst@gmail.com>
2021-12-14 14:19:42 -0300

    Chamar a tela inicial do appClip na content view dele

--- AppClipEventInfo/ContentView.swift
+++ AppClipEventInfo/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        EventInfoView(viewModel: EventInfoViewModel(event: EventMock.event))
+        SearchView(viewModel: ExploreScreenViewModel(eventRepository: EventRepositoryMock(), gameRepository: GameRepositoryMock()))
     }
 }
 
